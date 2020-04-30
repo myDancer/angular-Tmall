fc26e0139e9c20b43271293e11a2809e8dfc1f8f
myDancer <326509877@qq.com>
2020-04-30 17:47:02 +0800

    详情

--- src/app/pages/product-detail/detail/detail.component.ts
+++ src/app/pages/product-detail/detail/detail.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
-
+  sizes = [38, 39, 40, 41, 42, 43, 44];
+  colors = [
+    {
+      productImg: '//img.alicdn.com/imgextra/i2/2269753886/TB2JIGEfl8lpuFjSspaXXXJKpXa_!!2269753886.jpg_40x40q90.jpg',
+      name: '5036立领藏青色'
+    }
+  ]
+  chooseIndex: number
+  chooseColorIndex: number
   constructor() { }
 
   ngOnInit() {
   }
-
+  chooseSize(size) {
+    this.chooseIndex = this.sizes.indexOf(size);
+  }
+  chooseColor(size) {
+    this.chooseColorIndex = this.sizes.indexOf(size);
+  }
 }
